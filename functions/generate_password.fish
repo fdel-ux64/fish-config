f830c8e8bb2742ea73c9c147977dc77b3172375c
fdel-ux64 <fdel@ik.me>
2025-12-26 17:48:59 +0100

    Add checks on generate_password fish function

--- functions/generate_password.fish
+++ functions/generate_password.fish
@@ -1,10 +1,25 @@
 function generate_password
-    # Prompt for the password length
-    read --prompt-str "Enter password length: " xx
+    # If arguments are given, use them instead of prompting
+    set -l xx $argv[1]
+    set -l zz $argv[2]
 
-    # Prompt for the number of passwords
-    read --prompt-str "Enter number of passwords: " zz
+    # Check if pwgen is available
+    if not type -q pwgen
+        echo "Error: The package 'pwgen' is required to generate passwords, but it is not installed."
+        return 1
+    end
 
-    # Generate the password using pwgen
-    pwgen -syc $xx $zz
+    # If length not provided, prompt for it
+    if test -z "$xx"
+        read --prompt-str "Enter password length: " xx
+    end
+
+    # If count not provided, prompt for it
+    if test -z "$zz"
+        read --prompt-str "Enter number of passwords: " zz
+    end
+
+    # Generate passwords, one per line
+    pwgen -1 -s -y -c $xx $zz
 end
+
