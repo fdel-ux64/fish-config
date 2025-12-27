6a2d3449181efd77cf32def5841eade8dbe07171
fdel-ux64 <fdel@ik.me>
2025-12-28 00:09:01 +0100

    Sync tested functions and completions from .config/fish

--- functions/shisto.fish
+++ functions/shisto.fish
@@ -1,9 +1,14 @@
 function shisto --description "Search fish history (optionally with a query)"
     if test (count $argv) -eq 0
         history
-    else
-        set -l query (string join ' ' -- $argv)
+        return
+    end
+
+    set -l query (string join ' ' -- $argv)
+
+    if type -q rg
         history | rg -i -- "$query"
+    else
+        history | grep -i -- "$query"
     end
 end
-
