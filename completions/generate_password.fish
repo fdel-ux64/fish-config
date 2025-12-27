a16155240077e5582bded98c49fc97fb82ea7565
fdel-ux64 <fdel@ik.me>
2025-12-27 09:20:45 +0100

    improve generate_password fish completion

--- completions/generate_password.fish
+++ completions/generate_password.fish
@@ -1,11 +1,12 @@
-# Disable file completion
+# Fisher completion for generate_password
+
+# Command itself
 complete -c generate_password -f
 
-# Help flag
+# Help flags
 complete -c generate_password -s h -l help -d "Show help"
 
-# First argument: password length
-complete -c generate_password -n "not __fish_seen_subcommand_from" -a "16 20 32 64" -d "Password length"
-
-# Second argument: number of passwords
-complete -c generate_password -n "__fish_seen_argument_from 16 20 32 64" -a "1 2 3 5 10" -d "Number of passwords"
+# Optional numeric arguments (length and count)
+# Fish does not provide positional argument completion directly, but we can hint with descriptions
+complete -c generate_password -n '__fish_use_subcommand' -a '16 20 32' -d 'Suggested password lengths'
+complete -c generate_password -n '__fish_use_subcommand' -a '1 5 10' -d 'Number of passwords to generate'
